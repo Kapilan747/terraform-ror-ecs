d70a3c87869c010ccbd6421dfe9f17d854c9bada
kapilan747 <kapilan357@gmail.com>
2026-06-09 06:49:56 +0000

    Add ECS service CPU autoscaling

---
+++ infra/terraform/autoscaling.tf
@@ -0,0 +1,29 @@
+resource "aws_appautoscaling_target" "ecs_service" {
+  max_capacity       = var.ecs_max_capacity
+  min_capacity       = var.ecs_min_capacity
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  depends_on = [
+    aws_ecs_service.app
+  ]
+}
+
+resource "aws_appautoscaling_policy" "ecs_cpu_target_tracking" {
+  name               = "${var.project_name}-cpu-target-tracking"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_service.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_service.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_service.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value       = 20
+    scale_in_cooldown  = 120
+    scale_out_cooldown = 60
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}
