07fd0ef5cc642ab3ad35bc28da9999a7bb8d711c
kapilan747 <kapilan357@gmail.com>
2026-06-09 05:25:34 +0000

    Add ACM certificate for Terraform RoR domain

---
+++ infra/terraform/domain.tf
@@ -0,0 +1,44 @@
+data "aws_route53_zone" "main" {
+  name         = var.route53_zone_name
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "app" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name    = var.domain_name
+    Project = var.project_name
+  }
+}
+
+resource "aws_route53_record" "app_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.app.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "app" {
+  certificate_arn         = aws_acm_certificate.app.arn
+  validation_record_fqdns = [for record in aws_route53_record.app_cert_validation : record.fqdn]
+}
+
+resource "aws_lb_listener_certificate" "app" {
+  listener_arn    = var.https_listener_arn
+  certificate_arn = aws_acm_certificate_validation.app.certificate_arn
+}
