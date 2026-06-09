c80838205e16cfa04fb9023ea7589980b16056cd
kapilan747 <kapilan357@gmail.com>
2026-06-09 04:51:36 +0000

    Add ECS cluster ASG and capacity provider

---
+++ infra/terraform/ecs.tf
@@ -0,0 +1,143 @@
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name    = "${var.project_name}-cluster"
+    Project = var.project_name
+  }
+}
+
+resource "aws_launch_template" "ecs" {
+  name_prefix   = "${var.project_name}-lt-"
+  image_id      = data.aws_ssm_parameter.ecs_optimized_ami.value
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.ecs_instance_sg.id
+  ]
+
+  user_data = base64encode(<<-EOF_USER_DATA
+    #!/bin/bash
+    echo "ECS_CLUSTER=${aws_ecs_cluster.main.name}" >> /etc/ecs/ecs.config
+    echo "ECS_ENABLE_CONTAINER_METADATA=true" >> /etc/ecs/ecs.config
+  EOF_USER_DATA
+  )
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name    = "${var.project_name}-ecs-instance"
+      Project = var.project_name
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = {
+      Name    = "${var.project_name}-ecs-volume"
+      Project = var.project_name
+    }
+  }
+
+  tags = {
+    Name    = "${var.project_name}-launch-template"
+    Project = var.project_name
+  }
+}
+
+resource "aws_autoscaling_group" "ecs" {
+  name                = "${var.project_name}-asg"
+  vpc_zone_identifier = var.subnet_ids
+
+  min_size         = var.asg_min_size
+  desired_capacity = var.asg_desired_capacity
+  max_size         = var.asg_max_size
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.ecs.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-ecs-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Project"
+    value               = var.project_name
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = "true"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "${var.project_name}-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      status                    = "ENABLED"
+      target_capacity           = 100
+      minimum_scaling_step_size = 1
+      maximum_scaling_step_size = 1
+      instance_warmup_period    = 300
+    }
+  }
+
+  tags = {
+    Name    = "${var.project_name}-capacity-provider"
+    Project = var.project_name
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = [
+    aws_ecs_capacity_provider.main.name
+  ]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    weight            = 1
+    base              = 1
+  }
+}
